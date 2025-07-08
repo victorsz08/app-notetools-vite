55e22087771c1c6e52244c413601e7ae2313e37d
victorsz08 <joaovictorsal777@gmail.com>
2025-07-08 12:48:24 -0300

    add avatar menu layout

---
+++ src/components/avatar/avatar-menu.tsx
@@ -0,0 +1,57 @@
+import { useAuth } from "@/context/auth-context";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
+import { LogOut, UserRound } from "lucide-react";
+import { Separator } from "../ui/separator";
+
+export function AvatarMenu() {
+  const { user, logout } = useAuth();
+
+  const getInitials = (firstName?: string, lastName?: string) => {
+    if (firstName && lastName) {
+      return `${firstName.charAt(0).toUpperCase()}${lastName
+        .charAt(0)
+        .toUpperCase()}`;
+    }
+    return "";
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Avatar className="w-8 h-8 cursor-pointer">
+          <AvatarImage src={user?.avatarUrl} alt={user?.username} />
+          <AvatarFallback>
+            {getInitials(user?.firstName, user?.lastName)}
+          </AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[240px]">
+        <DropdownMenuItem className="flex flex-col justify-start items-start gap-0 hover:bg-transparent">
+          <span className="font-semibold text-muted-foreground">
+            {user?.firstName} {user?.lastName}
+          </span>
+          <span className="text-xs text-muted-foreground/70">
+            {user?.username}
+          </span>
+        </DropdownMenuItem>
+        <Separator />
+        <DropdownMenuItem className="text-muted-foreground">
+          <a href="/perfil" className="flex justify-start items-center gap-1">
+            <UserRound className="w-2 h-2" />
+            <span>Perfil</span>
+          </a>
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => logout()} className="text-destructive hover:text-destructive flex justify-start items-center gap-1">
+            <LogOut className="w-2 h-2 text-destructive" />
+            <span>Sair</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,23 +3,22 @@ import { SidebarProvider } from "../ui/sidebar"
 import AppSidebar from "../sidebar/app-sidebar"
 import { ToggleSidebar } from "../sidebar/toggle-sidebar";
 import { Toaster } from "sonner";
-
-
+import { AvatarMenu } from "../avatar/avatar-menu";
 
 export function Layout() {
-
-    return (
-      <SidebarProvider>
-        <AppSidebar collapsible="icon" variant="sidebar" />
-        <main className="w-full">
-          <header className="flex px-4 items-center justify-between h-20 bg-sidebar border-b border-border/60 w-full">
-            <div>
-                <ToggleSidebar/>
-            </div>
-          </header>
-          <Outlet />
-        </main>
-        <Toaster richColors/>
-      </SidebarProvider>
-    );
+  return (
+    <SidebarProvider>
+      <AppSidebar collapsible="icon" variant="sidebar" />
+      <main className="w-full">
+        <header className="flex px-4 items-center justify-between h-20 bg-sidebar border-b border-border/60 w-full">
+          <div>
+            <ToggleSidebar />
+          </div>
+          <AvatarMenu />
+        </header>
+        <Outlet />
+      </main>
+      <Toaster richColors />
+    </SidebarProvider>
+  );
 };
