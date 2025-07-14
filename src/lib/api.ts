46c2f91ba7a88fcfb496c34026756cec3ac09e95
victorsz08 <joaovictorsal777@gmail.com>
2025-07-14 10:51:39 -0300

    fix url api axios

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8000/";
+const BASE_URL = "https://api-notetools-node.vercel.app/";
 
 const api = axios.create({
   baseURL: BASE_URL,
